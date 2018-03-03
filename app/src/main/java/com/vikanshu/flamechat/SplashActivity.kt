d8586303d24cdd2736f96b83941790e2f63e283a
vikanshu <vikanshu2016@gmail.com>
2018-03-04 00:30:17 +0530

    added permissions

--- app/src/main/java/com/vikanshu/flamechat/SplashActivity.kt
+++ app/src/main/java/com/vikanshu/flamechat/SplashActivity.kt
@@ -1,12 +1,78 @@
 package com.vikanshu.flamechat
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+
+    private val permissions = arrayOf(Manifest.permission.INTERNET,Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        if (!hasAllPermissions(this,*permissions)) // if permissions not granted then ask for them
+            ActivityCompat.requestPermissions(this,permissions,21023)
+        else // if permissions granted then move forward
+            move()
+    }
+
+    // function for result of asked permissions
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode){
+            21023 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED){
+                    move()
+                }else{
+                    showToast("Please grant all the permissions to continue")
+                    this.finish()
+                }
+            }
+        }
+    }
+
+    // function for have account button pressed
+    fun alreadyHaveAccountPressed(v: View){
+        // move to log in activity
+    }
+
+    // function for need account pressed
+    fun needAccountPressed(v: View){
+        // move to create account activity
+    }
+
+    // function to move or stay
+    private fun move(){
+        if (FirebaseAuth.getInstance().currentUser != null){
+            // move to main screen
+        }else{
+            // stay here
+        }
+    }
+
+    // function to show a toast
+    private fun showToast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
+    // function to check whether all permissions are granted
+    private fun hasAllPermissions(context: Context,vararg  permissions: String): Boolean{
+        for (i in permissions){
+            val res = context.checkCallingOrSelfPermission(i)
+            if (res == PackageManager.PERMISSION_DENIED)
+                return false
+        }
+        return true
     }
 }
