e192da7d86e7d21587b84685f61e3aa9df77c8b6
vikanshu <vikanshu2016@gmail.com>
2018-03-04 21:06:06 +0530

    added change status functionality

--- app/src/main/java/com/vikanshu/flamechat/MyProfileActivity.kt
+++ app/src/main/java/com/vikanshu/flamechat/MyProfileActivity.kt
@@ -1,16 +1,16 @@
 package com.vikanshu.flamechat
 
 import android.app.ProgressDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.text.Editable
+import android.text.TextUtils
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
@@ -22,17 +22,17 @@ import kotlinx.android.synthetic.main.activity_my_profile.*
 
 class MyProfileActivity : AppCompatActivity() {
 
-    private var profileImage: CircleImageView ?= null
-    private var username: TextView ?= null
-    private var status: TextView ?= null
-    private var tick: ImageView?= null
-    private var firebaseAuth: FirebaseAuth ?= null
-    private var firebaseUser: FirebaseUser ?= null
-    private var firebaseDatabase: DatabaseReference ?= null
-    private var firebaseStorage: StorageReference ?= null
-    private var alertBox: AlertDialog.Builder ?= null
-    private var progressDialog: ProgressDialog ?= null
-    private val databaseListener = object : ValueEventListener{
+    private var profileImage: CircleImageView? = null
+    private var username: TextView? = null
+    private var status: TextView? = null
+    private var tick: ImageView? = null
+    private var firebaseAuth: FirebaseAuth? = null
+    private var firebaseUser: FirebaseUser? = null
+    private var firebaseDatabase: DatabaseReference? = null
+    private var firebaseStorage: StorageReference? = null
+    private var alertBox: AlertDialog.Builder? = null
+    private var progressDialog: ProgressDialog? = null
+    private val databaseListener = object : ValueEventListener {
         override fun onDataChange(p0: DataSnapshot?) {
             username?.text = p0?.child("username")?.value.toString()
             status?.text = p0?.child("status")?.value.toString()
@@ -41,6 +41,7 @@ class MyProfileActivity : AppCompatActivity() {
             else Picasso.with(this@MyProfileActivity).load(Uri.parse(image))
                     .placeholder(R.drawable.default_avatar).into(profileImage)
         }
+
         override fun onCancelled(p0: DatabaseError?) {
             // in case we are unable to fetch data then warn user that saved values are loaded
             showToast("Network error. Showing offline saved values")
@@ -50,31 +51,33 @@ class MyProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
-        overridePendingTransition(0,0)
+        overridePendingTransition(0, 0)
         bottom_navigation?.selectedItemId = R.id.profile
         bottom_navigation?.setOnNavigationItemSelectedListener {
-            when (it.itemId){
+            when (it.itemId) {
                 R.id.friends -> {
-                    startActivity(Intent(this,FriendsActivity::class.java))
+                    startActivity(Intent(this, FriendsActivity::class.java))
                     this.finish()
                     true
                 }
                 R.id.chats -> {
-                    startActivity(Intent(this,AllChatsActivity::class.java))
+                    startActivity(Intent(this, AllChatsActivity::class.java))
                     this.finish()
                     true
                 }
                 R.id.add_friend -> {
-                    startActivity(Intent(this,AddFriendActivity::class.java))
+                    startActivity(Intent(this, AddFriendActivity::class.java))
                     this.finish()
                     true
                 }
                 R.id.requests -> {
-                    startActivity(Intent(this,RequestsActivity::class.java))
+                    startActivity(Intent(this, RequestsActivity::class.java))
                     this.finish()
                     true
                 }
-                else -> {false}
+                else -> {
+                    false
+                }
             }
         }
 
@@ -85,18 +88,46 @@ class MyProfileActivity : AppCompatActivity() {
         progressDialog = ProgressDialog(this)
         progressDialog?.setCancelable(false)
         alertBox = AlertDialog.Builder(this)
-
         // firebase variables
         firebaseAuth = FirebaseAuth.getInstance()
         firebaseUser = firebaseAuth?.currentUser
         firebaseDatabase = FirebaseDatabase.getInstance().reference.child("USERS")?.child(firebaseUser?.uid)
         firebaseStorage = FirebaseStorage.getInstance().reference.child("profile_images")
         firebaseDatabase?.addValueEventListener(databaseListener)
+        if (firebaseUser!!.isEmailVerified) tick?.setImageResource(R.drawable.green_tick)
+        else tick?.setImageResource(R.drawable.red_tick)
     }
 
     // function for change status pressed
-    fun changeStatus(v: View){
-
+    fun changeStatus(v: View) {
+        val view = layoutInflater.inflate(R.layout.single_edittext_alert, null)
+        val input = view.findViewById<EditText>(R.id.single_input)
+        input.setText(status?.text)
+        input.setSelection(input.length())
+        alertBox?.setView(view)
+        alertBox?.setMessage("Enter New Status")
+        alertBox?.setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
+        alertBox?.setPositiveButton("Update") { dialog, _ ->
+            if (TextUtils.isEmpty(input.text.toString())) {
+                dialog.dismiss()
+            } else {
+                progressDialog?.setMessage("Please Wait. Updating ......")
+                progressDialog?.show()
+                firebaseDatabase?.child("status")?.setValue(input?.text.toString())?.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        dialog.dismiss()
+                        progressDialog?.dismiss()
+                        status?.text = input.text.toString()
+                        showToast("Status updates successfully")
+                    } else {
+                        dialog.dismiss()
+                        progressDialog?.dismiss()
+                        showToast(it.exception?.message.toString())
+                    }
+                }
+            }
+        }
+        alertBox?.show()
     }
 
     override fun onPause() {
