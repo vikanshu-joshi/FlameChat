c7220a0bc9049360742b40f112355d7b0a92aa6e
vikanshu <vikanshu2016@gmail.com>
2018-03-17 22:29:14 +0530

    added send message functionality

--- app/src/main/java/com/vikanshu/flamechat/ChatActivity.kt
+++ app/src/main/java/com/vikanshu/flamechat/ChatActivity.kt
@@ -4,14 +4,17 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.HashMap
 
 class ChatActivity : AppCompatActivity() {
 
@@ -25,6 +28,8 @@ class ChatActivity : AppCompatActivity() {
     private var onlineStatus: DatabaseReference ?= null
     private var onlineLight: ImageView?= null
     private var myUid = ""
+    private var msgEditText: EditText ?= null
+    private var message: String ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +39,19 @@ class ChatActivity : AppCompatActivity() {
         setImage = findViewById(R.id.userImageChatScreen)
         setOnlineStatus = findViewById(R.id.onlineStatusChatScreen)
         onlineLight = findViewById(R.id.onlineLightChatScreen)
+        msgEditText = findViewById(R.id.messageChatScreen)
 
         val extras = intent.extras
         uid = extras["uid"].toString()
         username = extras["username"].toString()
         image = extras["image"].toString()
         myUid = FirebaseAuth.getInstance().uid.toString()
-        database = FirebaseDatabase.getInstance().reference.child("MESSAGES").child(myUid).child(uid)
+        database = FirebaseDatabase.getInstance().reference.child("MESSAGES")
         onlineStatus = FirebaseDatabase.getInstance().reference.child("ONLINE_STATUS").child(uid)
         onlineStatus?.addValueEventListener(object: ValueEventListener{
-            override fun onCancelled(p0: DatabaseError?) {}
+            override fun onCancelled(p0: DatabaseError?) {
+                setOnlineStatus?.text = "offline"
+            }
             override fun onDataChange(p0: DataSnapshot?) {
                 if (p0?.value == "online") { onlineLight?.setImageResource(R.drawable.ic_online);setOnlineStatus?.text = "online"}
                 else {
@@ -58,7 +66,6 @@ class ChatActivity : AppCompatActivity() {
         if (image == "default") setImage?.setImageResource(R.drawable.default_avatar)
         else Picasso.with(this@ChatActivity).load(Uri.parse(image)).placeholder(R.drawable.loading).into(setImage)
 
-
     }
 
 
@@ -67,6 +74,32 @@ class ChatActivity : AppCompatActivity() {
         super.onBackPressed()
     }
 
-    fun sendMessageBtn(v: View){}
+    fun sendMessageBtn(v: View){
+        message = msgEditText?.text.toString()
+        if (message != null && message != ""){
+            val msgMap = HashMap<String,Any>()
+            msgMap["from"] = "him"
+            msgMap["time"] = ServerValue.TIMESTAMP
+            msgMap["text"] = message as Any
+            val msgId = ('a'..'z').randomString(5)
+            database?.child(uid)?.child(myUid)?.child(msgId)?.setValue(msgMap)?.addOnCompleteListener {
+                if (it.isSuccessful){
+                    msgMap["from"] = "me"
+                    database?.child(myUid)?.child(uid)?.child(msgId)?.setValue(msgMap)?.addOnFailureListener {
+                        Toast.makeText(this,"Unable to send message",Toast.LENGTH_LONG).show()
+                    }
+                    msgEditText?.text?.clear()
+                }else{
+                    Toast.makeText(this,"Unable to send message",Toast.LENGTH_LONG).show()
+                }
+            }
+
+
+        }
+    }
+
+    private fun ClosedRange<Char>.randomString(length: Int) =
+            (1..length).map { (Random().nextInt(endInclusive.toInt() - start.toInt()) + start.toInt()).toChar() }
+                    .joinToString("")
 
 }
