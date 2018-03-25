f56b9fd59fe86367e7432eaafcede41f4982ff1c
vikanshu <vikanshu2016@gmail.com>
2018-03-25 22:20:14 +0530

    added all chats list

--- app/src/main/java/com/vikanshu/flamechat/BottomNavigationActivities/AllChatsActivity.kt
+++ app/src/main/java/com/vikanshu/flamechat/BottomNavigationActivities/AllChatsActivity.kt
@@ -1,11 +1,15 @@
 package com.vikanshu.flamechat.BottomNavigationActivities
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import com.jenzz.appstate.AppState
 import com.jenzz.appstate.AppStateListener
 import com.jenzz.appstate.AppStateMonitor
 import com.vikanshu.flamechat.AllUsersData
@@ -15,6 +19,17 @@ import com.vikanshu.flamechat.BottomNavigationActivities.AllChatsActivity.Static
 import com.vikanshu.flamechat.FriendsData
 import com.vikanshu.flamechat.R
 import kotlinx.android.synthetic.main.activity_all_chats.*
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.squareup.picasso.Picasso
+import com.vikanshu.flamechat.ChatActivity
+import kotlinx.android.synthetic.main.chat_user_layout.view.*
+
 
 class AllChatsActivity : AppCompatActivity() {
 
@@ -68,9 +83,40 @@ class AllChatsActivity : AppCompatActivity() {
             }
         }
     }
-
     private var appState: AppStateMonitor ?= null
+    private var chatRecycler: RecyclerView ?= null
+    val query = FirebaseDatabase.getInstance()
+            .reference
+            .child("CHATS")
+            .child(FirebaseAuth.getInstance().currentUser?.uid)
+            .limitToLast(Integer.MAX_VALUE)
+    val options = FirebaseRecyclerOptions.Builder<Chat>()
+            .setQuery(query, Chat::class.java)
+            .build()
+    val adapter = object : FirebaseRecyclerAdapter<Chat, ChatHolder>(options) {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatHolder {
+            // Create a new instance of the ViewHolder, in this case we are using a custom
+            // layout called R.layout.message for each item
+            val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.chat_user_layout, parent, false)
+            return ChatHolder(view,this@AllChatsActivity)
+        }
 
+        override fun onBindViewHolder(holder: ChatHolder, position: Int, model: Chat) {
+            holder.setName(model.getname())
+            holder.setLast(model.getlast())
+            holder.setImage(model.getimage())
+            holder.content_holder?.setOnClickListener {
+                val bundle = Bundle()
+                bundle.putString("username",model.getname())
+                bundle.putString("image",model.getimage())
+                bundle.putString("uid",model.getuid())
+                val intent = Intent(this@AllChatsActivity,ChatActivity::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_chats)
@@ -104,13 +150,62 @@ class AllChatsActivity : AppCompatActivity() {
         appState = AppStateMonitor.create(application)
         appState?.addListener(appStateMonitorListener)
         appState?.start()
+        chatRecycler = findViewById(R.id.chats_recyler_view)
+        chatRecycler?.setHasFixedSize(true)
+        chatRecycler?.layoutManager = LinearLayoutManager(this@AllChatsActivity)
+        chatRecycler?.adapter = adapter
     }
     override fun onStart() {
         super.onStart()
+        adapter.startListening()
         val usersDataFirebase = FirebaseDatabase.getInstance().reference.child("USERS")
         val friendsDataFirebase = FirebaseDatabase.getInstance().reference
                 .child("FRIENDS")?.child(FirebaseAuth.getInstance().uid)
         usersDataFirebase.addChildEventListener(listener)
         friendsDataFirebase?.addChildEventListener(friendsListener)
+    }
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+    class ChatHolder(itemView: View, var context: Context) : RecyclerView.ViewHolder(itemView) {
+        private val imageView = itemView.findViewById<ImageView>(R.id.image_chat_view)
+        val content_holder = itemView.findViewById<ConstraintLayout>(R.id.chat_main)
+        fun setName(name: String) {
+            itemView.name_chat_view.text = name
+        }
+        fun setLast(status: String) {
+            itemView.last_chat_view.text = status
+        }
+        fun setImage(image: String) {
+            if (image == "default") imageView.setImageResource(R.drawable.default_avatar)
+            else Picasso.with(context).load(Uri.parse(image)).placeholder(R.drawable.loading).into(imageView)
+        }
+    }
+    class Chat{
+        private var name = ""
+        private var image = ""
+        private var last = ""
+        private var uid = ""
+
+
+        constructor(){}
+
+        constructor(mname: String,mimage: String,mlast: String,muid: String){
+            this.name = mname
+            this.image = mimage
+            this.last = mlast
+            this.uid = muid
+        }
+
+        fun getname(): String {return this.name}
+        fun getimage(): String {return this.image}
+        fun getlast(): String {return this.last}
+        fun getuid(): String {return this.uid}
+
+        fun setname(mname: String){ this.name = mname }
+        fun setimage(mimage: String){ this.image = mimage }
+        fun setlast(mlast: String){ this.last = mlast }
+        fun setuid(muid: String){ this.uid = muid }
     }
 }
